1c0279f6927f58d5b56038b238c39b57ca211a4e
elfamossomatador <marchal.louis10@gmail.com>
2023-01-04 23:09:58 +0100

    tweaked app for railway

--- DocBlog/settings.py
+++ DocBlog/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iwp%u)jpuum(^61^*s&vsnr-xqw7&!l_(12*2-%#zttg@3e5^*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
