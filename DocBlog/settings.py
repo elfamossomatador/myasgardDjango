d3de9b6408c9cc9a924ae29d442a8ee2d69082fa
elfamossomatador <marchal.louis10@gmail.com>
2023-01-04 18:47:52 +0100

    tweaked app for heroku

--- DocBlog/settings.py
+++ DocBlog/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'DocBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'CaJK0lkoMYu2EOB6',
+        'HOST': 'db.rolragpkcbjzcaggazru.supabase.co',
+        'PORT': '5432',
     }
 }
 
