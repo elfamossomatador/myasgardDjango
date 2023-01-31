6ae90a2db964f379f502901a3f7b97a9e97dd414
elfamossomatador <marchal.louis10@gmail.com>
2023-01-31 17:18:46 +0100

    vercel settings done

--- DocBlog/settings.py
+++ DocBlog/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "DocBlog/static")
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
